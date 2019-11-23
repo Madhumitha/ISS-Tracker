e9f99d55d31c456b885cedf7c014640a5854206e
Chris Malilay <malilaychris@gmail.com>
2019-11-23 03:33:05 -0600

    Premerge content

---
+++ script/dynamicMap.js
@@ -0,0 +1,71 @@
+// Locate moveISS
+
+function moveISS () {
+    $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
+
+    console.log(data);
+         let lat = data['iss_position']['latitude'];
+         let lon = data['iss_position']['longitude'];
+
+         // Icon Image 
+
+         let LeafIcon = L.Icon.extend({
+            options: {
+               iconSize:     [30, 30],
+               shadowSize:   [50, 50],
+            }
+        });
+
+        let Icon = new LeafIcon({
+            iconUrl: 'http://open-notify.org/Open-Notify-API/map/ISSIcon.png',
+            shadowUrl: 'http://open-notify.org/Open-Notify-API/map/ISSIcon_shadow.png'
+        })
+
+         // Create variable to hold map element, give initial settings to map
+         let map = L.map('map',{ center: [lat, lon], zoom: 14});
+
+         //Add OpenStreetMap tile layer to map element
+         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap' }).addTo(map);
+
+         map.setView([0, 0], 0);
+
+         let iss = L.marker([lat, lon],{icon: Icon}).addTo(map).bindPopup("<b>The International Space Station</b>").openPopup();
+         let isscirc = L.circle([lat, lon], 500, {
+           color: 'red',
+           fillColor: '#f03',
+           fillOpacity: 0.5
+         }).addTo(map);
+        iss.setLatLng([lat, lon]);
+        console.log(iss.setLatLng([lat, lon]));
+        isscirc.setLatLng([lat, lon]);
+        console.log(isscirc.setLatLng([lat, lon]));
+        map.panTo([lat, lon], animate=true);
+        console.log(map.panTo([lat, lon], animate=true))
+    });
+    setTimeout(moveISS, 5000);
+}
+
+// Number of People in Space 
+
+function numberPeopleSpace() {
+
+    $.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
+    
+    console.log("Number of People in Space");
+    console.log(data);
+    
+    let number = data['number'];
+    $('#spacepeeps').html(number);
+
+    data['people'].forEach(function (d) {
+        console.log(d.name);
+        let astronames = d.name;
+         $('#astronames').append('<li>' + astronames + '</li>');
+    });
+});
+}
+
+// Function Calls 
+
+moveISS();
+numberPeopleSpace();
